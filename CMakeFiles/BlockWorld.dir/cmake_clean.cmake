cb05621124b0584e429e670740f5848951e36258
Sameer <sameer.al.harbi2000@gmail.com>
2025-10-11 02:25:39 +0100

    Import ported project

---
+++ CMakeFiles/BlockWorld.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BlockWorld.dir/link.d"
+  "BlockWorld"
+  "BlockWorld.pdb"
+  "CMakeFiles/BlockWorld.dir/src/BlockWorld.cpp.o"
+  "CMakeFiles/BlockWorld.dir/src/BlockWorld.cpp.o.d"
+  "CMakeFiles/BlockWorld.dir/src/ChunkBlock.cpp.o"
+  "CMakeFiles/BlockWorld.dir/src/ChunkBlock.cpp.o.d"
+  "CMakeFiles/BlockWorld.dir/src/ModelLoader/tiny_loader_texture.cpp.o"
+  "CMakeFiles/BlockWorld.dir/src/ModelLoader/tiny_loader_texture.cpp.o.d"
+  "CMakeFiles/BlockWorld.dir/src/cube_tex.cpp.o"
+  "CMakeFiles/BlockWorld.dir/src/cube_tex.cpp.o.d"
+  "CMakeFiles/BlockWorld.dir/src/glad.c.o"
+  "CMakeFiles/BlockWorld.dir/src/glad.c.o.d"
+  "CMakeFiles/BlockWorld.dir/src/wrapper_glfw.cpp.o"
+  "CMakeFiles/BlockWorld.dir/src/wrapper_glfw.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/BlockWorld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
